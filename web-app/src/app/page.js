e1bf779a80b1987b04c684bdf9fcb5c010b18a25
Matyáš Trnka <hello@matrn.dev>
2025-09-18 01:44:06 +0200

    edit main page

--- web-app/src/app/page.js
+++ web-app/src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      sorry, this page is under construction :/
+    <main className="m-3">
+      <p>sorry, nothing here yet!</p>
       <p className="text-primary fs-1"><a href="/play">play game</a></p>
     </main>
   );
