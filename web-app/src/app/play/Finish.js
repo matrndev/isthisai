ebf91fa9162092adf2d65b07154be762ac2d1aba
Matyáš Trnka <hello@matrn.dev>
2025-09-24 00:21:16 +0200

    add note on score

--- web-app/src/app/play/Finish.js
+++ web-app/src/app/play/Finish.js
@@ -9,7 +9,9 @@ export default function Finish({ correctAnswers }) {
         <div className="text-center mt-5">
             <h1>Good game!</h1>
             <h4>You answered {correctAnswers} out of 5 questions correctly.</h4>
-            <p>Thanks for playing!</p>
+            {correctAnswers < 3 && <p>Try again, better luck next time! :/</p>}
+            {correctAnswers >= 3 && !correctAnswers > 3 && <p>Not bad! Thanks for playing!</p>}
+            {correctAnswers > 3 && <p>Oooh you&#39;re good at this! Thanks for playing!</p>}
             <button onClick={() => resetGame()} className="btn btn-primary btn-lg">Play Again</button>
         </div>
     )
