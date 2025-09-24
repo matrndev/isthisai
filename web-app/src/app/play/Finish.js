2bb57aa05dd484da296b5970f70528c979589f01
Matyáš Trnka <hello@matrn.dev>
2025-09-24 19:42:48 +0200

    reword game end page

--- web-app/src/app/play/Finish.js
+++ web-app/src/app/play/Finish.js
@@ -7,11 +7,11 @@ export default function Finish({ correctAnswers }) {
 
     return (
         <div className="text-center mt-5">
-            <h1>Good game!</h1>
+            <h1>Game over</h1>
             <h4>You answered {correctAnswers} out of 5 questions correctly.</h4>
-            {correctAnswers < 3 && <p>Try again, better luck next time! :/</p>}
-            {correctAnswers >= 3 && !correctAnswers > 3 && <p>Not bad! Thanks for playing!</p>}
-            {correctAnswers > 3 && <p>Oooh you&#39;re good at this! Thanks for playing!</p>}
+            {correctAnswers < 3 && <p>Try again, better luck next time :/</p>}
+            {correctAnswers >= 3 && correctAnswers <= 3 && <p>Not bad! Thanks for playing!</p>}
+            {correctAnswers > 3 && <p>Oooh, you&#39;re good at this! Thanks for playing!</p>}
             <button onClick={() => resetGame()} className="btn btn-primary btn-lg">Play Again</button>
         </div>
     )
